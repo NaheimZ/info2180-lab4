cffcbe61b7b3f387ff9327a7ef603b9f04a1fd32
Naheim Clarke <naheimclarke876@gmail.com>
2025-11-21 22:17:15 -0500

    Added app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("JS is connected");
+
+document.getElementById("searchBtn").addEventListener("click", function () {
+    fetch("superheroes.php")
+        .then(response => response.text())
+        .then(data => {
+            alert(data);
+        })
+        .catch(error => console.error("Error:", error));
+});
